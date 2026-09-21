150d371755fca4fd45bb7842421cfd5c0fc880bf
dung <ledung220402@gmail.com>
2026-09-21 16:38:30 +0700

    [feat] config view engine and request.body

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 //const express = require('express');
 import express from "express"; //import express
 import "dotenv/config"
-const app = express(); // tạo express application
+import webRoutes from "./routes/web";
 
+const app = express(); // tạo express application
 const PORT = process.env.PORT || 8888; //init port
 
-//tạo route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+//config view engine
+app.set('view engine', 'ejs');
+app.set('views',  __dirname + '/../src/views');
 
+//CONFIG request.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+webRoutes(app);
 //run server trên port đã khởi tạo trước đó
 //nạp các dữ liệu khai báo ở trên rồi chạy (ví dụ như route)
 app.listen(PORT, () => {
     console.log('Server started on port', process.env.PORT);
+    console.log(__dirname + '/../src/views');
 })
