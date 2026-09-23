65dca9c01d057c60fa72cd41d3a863829f67f105
dung <ledung220402@gmail.com>
2026-09-23 15:28:53 +0700

    [feat] add logic get all user service

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,5 +1,19 @@
+import mysql from "mysql2/promise";
+import getConnection from "../config/database";
+
 const handleCreateUserService = ( fullName: string, email: string, address: string) => {
     console.log("ok");
 }
 
-export { handleCreateUserService };
+const getAllUsersService = async () => {
+    const connection = await getConnection();
+    try {
+        const [results] = await connection.query(
+            'SELECT * FROM `users`'
+        );
+        return results
+    } catch (err) {
+        return [];
+    }
+}
+export { handleCreateUserService, getAllUsersService };
